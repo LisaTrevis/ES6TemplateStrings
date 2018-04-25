d6aa3aa7d10997d2617d7158aa6419cf65482863
LisaTrevis <l_trevis@hotmail.com>
2018-04-25 11:12:47 -0700

    Exercises completed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // ----- Template Strings ------
 
 // Also known as template literals or string interpolation
-
+// Really great when mixing in a lot of variables to clean up code.
 
 // function getMessage() {
 // 	const year = new Date().getFullYear();
@@ -21,4 +21,24 @@
 
 // function getMessage() {
 // 	return `The year is ${ new Date().getFullYear() }`;
+// }
+
+
+// Refactor the functions to use template strings:
+
+// function doubleMessage(number) {
+//   return "Your number doubled is " + (2 * number);
+// }
+
+// function doubleMessage(number) {
+//   return `Your number doubled is ${2 * number}`;
+// }
+
+
+// function fullName(firstName, lastName) {
+//   return firstName + lastName;
+// }
+
+// function fullName(firstName, lastName) {
+//   return `${firstName} ${lastName}`;
 // }
