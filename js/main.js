fa545e6e39eab0c5faef3972f39bdb7abf3585f7
LisaTrevis <l_trevis@hotmail.com>
2018-04-25 10:53:58 -0700

    Completed

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
 // ----- Template Strings ------
 
+// Also known as template literals or string interpolation
+
+
+// function getMessage() {
+// 	const year = new Date().getFullYear();
+
+// 	return "The year is " + year;
+// }
+
+
+// function getMessage() {
+// 	const year = new Date().getFullYear();
+
+// 	return `The year is ${year}`;
+// }
+
+
+// Can also be written as:
+
+// function getMessage() {
+// 	return `The year is ${ new Date().getFullYear() }`;
+// }
